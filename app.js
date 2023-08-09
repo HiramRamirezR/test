9c16873cc83c7f630ac2249d082edb7df6310ef6
hiramramirez <hiram.j.ramirez@gmail.com>
2023-08-09 11:33:22 -0500

    whatsapp added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const ubicacion = document.querySelector('.ubicacion');
+const whatsapp = document.querySelector('.whatsapp');
 
 ubicacion.addEventListener('mouseenter', () => {
   const confirmar = confirm('¿Abrir la ubicación?');
@@ -6,3 +7,17 @@ ubicacion.addEventListener('mouseenter', () => {
     window.open('https://goo.gl/maps/WdJPf3yKkvCC2bSH7', '_blank');
   }
 })
+
+ubicacion.addEventListener('click', () => {
+  const confirmar = confirm('¿Abrir la ubicación?');
+  if (confirmar) {
+    window.open('https://goo.gl/maps/WdJPf3yKkvCC2bSH7', '_blank');
+  }
+})
+
+whatsapp.addEventListener('mouseenter', () => {
+  const confirmar = confirm('¿Abrir Whatsapp?');
+  if (confirmar) {
+    window.open('https://api.whatsapp.com/send?phone=+522221033301', '_blank');
+  }
+});
